ee9d2ac78d1767bd4be16c68c46eaea4ca1b9380
Will Beasley <wibeasley@hotmail.com>
2016-04-25 16:09:06 -0500

    Usage Graphs

--- agendas/fteAnalysis.R
+++ agendas/fteAnalysis.R
@@ -20,6 +20,10 @@ fteData$Instance[fteData$institution %in% "OUHSC" & fteData$name %in% "Thomas"] 
 fteData$Instance[fteData$institution %in% "OUHSC" & fteData$name %in% "Pravina"] <- "Enterprise"
 fteData$Instance[fteData$institution %in% "OUHSC" & fteData$name %in% "TnP"] <- "BBMC+Enterprise"
 
+ouData$Instance[ouData$institution %in% "OUHSC" & ouData$name %in% "Thomas"] <- "BBMC"
+ouData$Instance[ouData$institution %in% "OUHSC" & ouData$name %in% "Pravina"] <- "Enterprise"
+ouData$Instance[ouData$institution %in% "OUHSC" & ouData$name %in% "TnP"] <- "BBMC+Enterprise"
+
 ggplot(fteData, aes(x=no_fte,y=no_cur_users)) + geom_point(aes(col=factor(OUdat)),show.legend = FALSE) + stat_smooth(method = "loess") + geom_text(aes(label=Instance,color=factor(OUdat)),hjust=.5,vjust=-.5,show.legend = FALSE) + theme_bw() + scale_color_manual(values=c("black","red"))
 ggplot(fteData, aes(x=no_fte,y=no_cur_prod_proj)) + geom_point() + stat_smooth(method = "lm")
 
@@ -28,3 +32,31 @@ ggplot(fteData[!fteData$no_fte %in% 10,], aes(x=no_fte,y=no_cur_prod_proj)) + ge
 
 ggplot(fteData, aes(x=no_fte2,y=no_cur_users,label=Instance)) + geom_point(aes(col=factor(OUdat)),show.legend = FALSE) + stat_smooth(method = "loess") + geom_text(aes(label=Instance,color=factor(OUdat)),hjust=.5,vjust=-.5,show.legend = FALSE) + theme_bw() + scale_color_manual(values=c("black","red"))
 ggplot(fteData, aes(x=no_fte2,y=no_cur_prod_proj)) + geom_point(aes(col=factor(OUdat)),show.legend = FALSE) + stat_smooth(method = "loess") + geom_text(aes(label=Instance,color=factor(OUdat)),hjust=.5,vjust=-.5,show.legend = FALSE) + theme_bw() + scale_color_manual(values=c("black","red"))
+
+ggplot(fteData, aes(x=no_cur_prod_proj, y=no_fte)) + 
+  geom_point() + 
+  geom_vline(xintercept=150, color="purple") +
+  stat_smooth(method = "lm") +
+  theme_light()
+  
+
+ggplot(fteData, aes(x=no_cur_prod_proj, y=no_fte)) + 
+  geom_smooth(method = "loess") + 
+  geom_point(aes(color=factor(OUdat), fill=factor(OUdat)),show.legend = FALSE, shape=21, size=7) + 
+  geom_hline(yintercept=.35, color="tomato"          , alpha=.7) +
+  geom_hline(yintercept=.95, color="blue"          , alpha=.7) +
+  geom_vline(xintercept=153, color="tomato"          , alpha=.7) +
+  geom_text(data=ouData, mapping=aes(label=Instance), color="tomato",hjust=c(.5, 1.1, -.1), size=5, vjust=2, check_overlap=F, show.legend = FALSE) + 
+  scale_x_continuous(labels=scales::comma) + 
+  scale_color_manual(values=c("gray20","tomato")) + 
+  scale_fill_manual(values=c("#22222222","#EE4422BB")) + 
+  coord_cartesian(xlim=c(-150, 1300)) +
+  theme_light() +
+  labs(x="Count of Projects in Production", y="Insititution-supported FTE (sum)")
+
+ggsave("project-count.png", width=7, height=5, units="in")
+
+last_plot() %+% 
+  aes(x=no_cur_users) +
+  labs(x="Count of Users")
+ggsave("user-count.png", width=7, height=5, units="in")
